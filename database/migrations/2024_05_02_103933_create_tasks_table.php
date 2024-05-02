de3554919cf2e1612b3ec1b02a54085b2b442bee
annequinox <zara.zannetta@gmail.com>
2024-05-02 17:51:58 +0700

    update database

--- database/migrations/2024_05_02_103933_create_tasks_table.php
+++ database/migrations/2024_05_02_103933_create_tasks_table.php
@@ -11,19 +11,22 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('taskmanager', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-
+            $table->string('taskName');
+            $table->string('taskCategory');
+            $table->dateTime('taskDueDate');
+            $table->text('taskDescription')->nullable();
+            $table->boolean('isCompleted')->default(false);
             $table->timestamps();
         });
     }
-
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('taskmanager');
+        Schema::dropIfExists('tasks');
     }
 };
