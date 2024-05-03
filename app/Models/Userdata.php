2d330d147d50294dcb36a0b5c6288050ae9c00f3
annequinox <zara.zannetta@gmail.com>
2024-05-03 12:13:04 +0700

    update signup

--- app/Models/Userdata.php
+++ app/Models/Userdata.php
@@ -2,10 +2,40 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Userdata extends Model
+class Userdata extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'username',
+        'password',
+    ];
+
+    /**
+     * hidden attribute
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
+     * cast attributes
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'password' => 'hashed',
+    ];
 }
---
+++ resources/views/signup.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TaskVenture|Sign Up</title>
+</head>
+<body>
+<main class="form-signup">
+    <form action="/signup" method="post">
+    @csrf
+    <h1>Create an account</h1>
+    
+        <div>
+        <label for="username">Username</label>
+        <input type="text" name="username" class="form-control" id="username" placeholder="Enter your username">
+        </div>
+
+        <div>
+        <label for="password">Password</label>
+        <input type="password" name="password" class="form-control" id="password" placeholder="Enter your password">
+        </div>
+
+        </label>
+        </div>
+
+        <button class="button-submit-signup" type="submit">Create an account</button>
+    </form>
+
+    <p>Already have an account? <a href="/login">Log in</a></p>
+</main>
+
+</body>
+</html>
